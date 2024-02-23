c73e0bc651d4a6fc54885721ada1ca01dcbefd4e
Ashrindy <honzula1000@gmail.com>
2024-02-23 18:24:57 +0100

    fixed a mistake pointed out by ik-01

--- 010Templates/Sonic/include.h
+++ 010Templates/Sonic/include.h
@@ -1,17 +1,17 @@
-#include "../ut/ResBinaryFileHeader.h"
-#include "../ut/array.h"
-#include "../ut/math.h"
-#include "../ut/string.h"
-#include "../ut/010editor.h"
-#include "../ut/variables.h"
-#include "../ut/reference.h"
+#include "ut/ResBinaryFileHeader.h"
+#include "ut/array.h"
+#include "ut/math.h"
+#include "ut/string.h"
+#include "ut/010editor.h"
+#include "ut/variables.h"
+#include "ut/reference.h"
 
 void GetStruct (string structName)
 {
 	switch (structName)
 	{
-		case "ResEffectDatabase":
-			struct ResEffectDatabase resEffectDatabase <name="ResEffetDatabase">;
+		case "ResParticleLocation":
+			struct ResParticleLocation resParticleLocation <name="ResParticleLocation">;
 			break;
 		
 		
