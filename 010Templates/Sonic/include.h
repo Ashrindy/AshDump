6fb1c8ca5486dab9c5b543c370876cbb8a4c74f3
Ashrindy <honzula1000@gmail.com>
2024-02-23 19:04:16 +0100

    basic template for .aism reading

--- 010Templates/Sonic/include.h
+++ 010Templates/Sonic/include.h
@@ -1,10 +1,10 @@
-#include "ut/ResBinaryFileHeader.h"
-#include "ut/array.h"
-#include "ut/math.h"
-#include "ut/string.h"
-#include "ut/010editor.h"
-#include "ut/variables.h"
-#include "ut/reference.h"
+#include "../ut/ResBinaryFileHeader.h"
+#include "../ut/array.h"
+#include "../ut/math.h"
+#include "../ut/string.h"
+#include "../ut/010editor.h"
+#include "../ut/variables.h"
+#include "../ut/reference.h"
 
 void GetStruct (string structName)
 {
@@ -14,6 +14,13 @@ void GetStruct (string structName)
 			struct ResParticleLocation resParticleLocation <name="ResParticleLocation">;
 			break;
 		
+		case "ResPhysicalSkeleton":
+			struct ResPhysicalSkeleton resPhysicalSkeleton <name="ResPhysicalSkeleton">;
+			break;
+
+		case "ResAIStateMachine":
+			struct ResAIStateMachine resAIStateMachine <name="ResAIStateMachine">;
+			break;
 		
 		default:
 			break;
